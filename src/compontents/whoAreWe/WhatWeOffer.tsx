3df6e64a27ae0b47d9a942e49a87cc15d01e22f6
Микола Ковальський <mykola200410@gmail.com>
2023-06-06 22:42:41 -0400

    fixeslint errors

--- src/compontents/whoAreWe/WhatWeOffer.tsx
+++ src/compontents/whoAreWe/WhatWeOffer.tsx
@@ -15,7 +15,7 @@ interface WhatWeOfferProps {
 
 
 
-let props = 'relative text-center md:text-left '
+let props = "relative text-center md:text-left "
 
 const WhatWeOffer: FC<WhatWeOfferProps> = ({ }) => {
     return <section id='offer'  className='scale-75 py-0  md:scale-100 text-slate-900  md:py-40  px-5'>
@@ -30,7 +30,7 @@ const WhatWeOffer: FC<WhatWeOfferProps> = ({ }) => {
         </div>
         <div className=' flex justify-evenly gap-20 flex-wrap'>
 
-            <div className={`${props} max-w-lg  `}> <Image className='absolute -left-10 scale-150 top-1.5 ' src={square} alt=''></Image>
+            <div className={`${props} max-w-lg  `}> <Image className='absolute -left-10 scale-150 top-1.5 ' src={square} alt='someinfo'></Image>
                 <h4 className=' mb-6 font-bold tracking-widest text-2xl'> User experience design</h4>
 
                 <FadeIn delay={0.5}>
@@ -38,7 +38,7 @@ const WhatWeOffer: FC<WhatWeOfferProps> = ({ }) => {
                     <p className=' text-lg tracking-tight leading-7 text-slate-900 '>We start by performing research and analysis to ensure we understand the problem and project goals. We create a multiple hypothesis and validate these. We create well defined information structures, user flows and prototypes and test these rapidly to make sure we are heading in the right direction. </p>
                 </FadeIn>
             </div>
-            <div className={`${props} max-w-lg `}> <Image className='absolute -left-10 scale-150 top-1.5 ' src={computer} alt=''></Image>
+            <div className={`${props} max-w-lg `}> <Image className='absolute -left-10 scale-150 top-1.5 ' src={computer} alt='someinfo'></Image>
                 <h4 className=' mb-6 font-bold tracking-widest text-2xl'>User interface design</h4>
 
 
@@ -49,20 +49,20 @@ const WhatWeOffer: FC<WhatWeOfferProps> = ({ }) => {
                 </FadeIn>
 
             </div>
-            <div className={`${props} max-w-lg `}> <Image className='absolute -left-10 scale-150 top-1.5 ' src={palm} alt=''></Image>
+            <div className={`${props} max-w-lg `}> <Image className='absolute -left-10 scale-150 top-1.5 ' src={palm} alt='someinfo'></Image>
                 <h4 className=' mb-6 font-bold tracking-widest text-2xl'>Interaction design</h4>
                 <FadeIn delay={0.2}>
                     <p className=' text-lg tracking-tight leading-7 text-slate-900 '>We create a dialogue between a person and a product, system or service. We aim to create designs that users will find easy to use. </p>
                 </FadeIn>
             </div>
-            <div className={`${props} max-w-lg `}> <Image className='absolute -left-10 scale-150 top-1.5 '  src={headPhones} alt=''></Image>
+            <div className={`${props} max-w-lg `}> <Image className='absolute -left-10 scale-150 top-1.5 '  src={headPhones} alt='someinfo'></Image>
                 <h4 className=' mb-6 font-bold tracking-widest text-2xl'>Long term support
                 </h4>
                 <FadeIn delay={0.2}>
-                    <p className=' text-lg tracking-tight leading-7 text-slate-900 '>We prefer to be in touch with our customers for a long time. We build strong and trusting relationships by supporting our customers' projects 24/7. </p>
+                    <p className=' text-lg tracking-tight leading-7 text-slate-900 '>We prefer to be in touch with our customers for a long time. We build strong and trusting relationships by supporting our customers&apos projects 24/7. </p>
                 </FadeIn>
             </div>
-            <div className={`${props} max-w-lg `}> <Image className='absolute -left-10 scale-150 top-1.5 ' src={star} alt=''></Image>
+            <div className={`${props} max-w-lg `}> <Image className='absolute -left-10 scale-150 top-1.5 ' src={star} alt='someinfo'></Image>
                 <h4 className=' mb-6 font-bold tracking-widest text-2xl'>Brand identity
                 </h4>
 
